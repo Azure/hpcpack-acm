a0ad9eb7888f57e66cab2a622779489ee1acd73e
Evan Cui <evanc@microsoft.com>
2018-02-26 17:51:49 +0800

    Enable configuration

---
+++ src/services/Common/CloudOption.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.HpcAcm.Services.Common
+{
+    public class CloudOption
+    {
+        public string StorageKeyOrSas { get; set; }
+        public int QueueServerTimeoutSeconds { get; set; }
+        public int TableServerTimeoutSeconds { get; set; }
+        public int QueueCount { get; set; }
+        public string TaskCompletionQueueName { get; set; }
+        public string RegisterTableName { get; set; }
+        public string HeartbeatTableName { get; set; }
+    }
+}
---
+++ src/services/JobDispatcher/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Microsoft.HpcAcm.Services.JobDispatcher
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.HpcAcm.Services.Common;
+
+    class Program
+    {
+        public static IConfigurationRoot Configuration;
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(@"E:\GitHub\hpc-acm\src\services\JobDispatcher")
+                .AddJsonFile("appconfig.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            Program.Configuration = builder.Build(); 
+
+            var cloudOption = Configuration.GetSection("CloudOption").Get<CloudOption>();
+
+            Console.WriteLine(cloudOption.StorageKeyOrSas);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
