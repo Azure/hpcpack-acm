5360e4dbd06320a37dde451c1fb65cbe311bbaba
evanc <dnetdvp@163.com>
2018-04-12 16:23:23 +0800

    added parameter definitions

--- src/Common/DTO/DiagnosticsTest.cs
+++ src/Common/DTO/DiagnosticsTest.cs
@@ -8,5 +8,7 @@ namespace Microsoft.HpcAcm.Common.Dto
         public string Category { get; set; }
         public string Arguments { get; set; }
         public string Description { get; set; }
+        public ParameterDefinition[] Parameters { get; set; }
+
     }
 }
---
+++ src/Common/DTO/ParameterDefinition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.HpcAcm.Common.Dto
+{
+    public class ParameterDefinition
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public object DefaultValue { get; set; }
+        public string[] Options { get; set; }
+    }
+}
