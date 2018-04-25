55356bd0582e8e7e554863726cf7311f7c6cfa09
Robert Zhang <louirobert@gmail.com>
2018-04-25 17:47:03 +0800

    Fix some issues in unit test.

--- src/portal/src/app/command/result-detail/result-detail.component.spec.ts
+++ src/portal/src/app/command/result-detail/result-detail.component.spec.ts
@@ -13,25 +13,45 @@ import { ResultDetailComponent } from './result-detail.component';
 @Component({ selector: 'app-back-button', template: '' })
 class BackButtonStubComponent {}
 
+class CommandApiStub {
+  private counter = 0;
+
+  constructor(public result, public content) {}
+
+  get() {
+    return of(this.result);
+  }
+
+  getOutput() {
+    let res = null;
+    switch(this.counter++) {
+      case 0:
+        res = of({ content: this.content, size: this.content.length, offset: 0 });
+        break;
+      case 1:
+        of({ content: undefined, size: 0, offset: this.content.length });
+        break;
+    }
+    return res;
+  }
+}
+
 class ApiServiceStub {
   static result = { command: 'TEST COMMAND', nodes: [{ name: 'TEST NODE', state: 'finished' }] };
 
   static outputContent = 'TEST CONTENT';
 
-  command = {
-    get: () => of(ApiServiceStub.result),
-    getOutput: () => of(
-      { content: ApiServiceStub.outputContent, size: ApiServiceStub.outputContent.length, offset: 0 },
-      { size: 0, offset: ApiServiceStub.outputContent.length }
-    ),
-  }
+  command = new CommandApiStub(ApiServiceStub.result, ApiServiceStub.outputContent);
 }
 
 const activatedRouteStub = {
   paramMap: of({ get: () => 1 })
 }
 
-fdescribe('ResultDetailComponent', () => {
+//TODO: Enable it when the error is fixed
+//Uncaught TypeError: Cannot read property 'nativeElement' of undefined
+// at .../src/app/command/result-detail/result-detail.component.ts.ResultDetailComponent.scrollOutputToBottom (_karma_webpack_/webpack:/opt/app/src/app/command/result-detail/result-detail.component.ts:225)
+describe('ResultDetailComponent', () => {
   let component: ResultDetailComponent;
   let fixture: ComponentFixture<ResultDetailComponent>;
 
