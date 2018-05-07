287e1979daf7dccede80adcb7e42f34a63c68bec
Robert Zhang <louirobert@gmail.com>
2018-05-07 16:39:18 +0800

    Enter result detail by link on id for diagnostic list.

--- src/portal/src/app/diagnostics/result-list/result-list.component.ts
+++ src/portal/src/app/diagnostics/result-list/result-list.component.ts
@@ -15,7 +15,7 @@ export class ResultListComponent implements OnInit, OnDestroy {
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   private dataSource = new MatTableDataSource();
-  private displayedColumns = ['select', 'id', 'test', 'diagnostic', 'category', 'progress', 'state', 'result', 'actions'];
+  private displayedColumns = ['select', 'id', 'test', 'diagnostic', 'category', 'progress', 'state', 'actions'];
 
   private selection = new SelectionModel(true, []);
   private interval: number;
@@ -178,10 +178,6 @@ export class ResultListComponent implements OnInit, OnDestroy {
       case 'Canceled': return 'cancel';
       default: return 'autonew';
     }
-  }
-
-  private getResult(id) {
-    this.router.navigate(['/diagnostics/results/' + id]);
   }
 
   applyFilter(text: string): void {
