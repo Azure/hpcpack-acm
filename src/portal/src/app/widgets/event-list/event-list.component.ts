a3801f8a9446d678cf70bef7ba9d9b98902e81bb
Jingjing Li <lijingjing1008@gmail.com>
2018-05-22 16:01:34 +0800

    Update event-list widget based on real data

--- src/portal/src/app/widgets/event-list/event-list.component.ts
+++ src/portal/src/app/widgets/event-list/event-list.component.ts
@@ -9,17 +9,21 @@ export class EventListComponent implements OnInit {
   @Input()
   events: any;
 
-  constructor() { }
+  constructor(
+  ) { }
 
   ngOnInit() {
+
   }
 
-  eventType(type) {
-    if (type == 'node event') {
-      return 'node-event';
+  eventType(source, type) {
+    let eventType = source + ' ' + type;
+
+    if (eventType == 'Node Information') {
+      return 'node-information';
     }
-    else if (type == 'Azure scheduled event') {
-      return 'azure-event';
+    else if (eventType == 'Node Warning') {
+      return 'node-warning';
     }
   }
 
