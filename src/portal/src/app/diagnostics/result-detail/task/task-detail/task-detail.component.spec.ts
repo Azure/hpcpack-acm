be832439a30dd0f68fa42de452aa45bd02b1a0d5
Jingjing Li <lijingjing1008@gmail.com>
2018-06-04 17:26:36 +0800

    Update unit test of result-detail

--- src/portal/src/app/diagnostics/result-detail/task/task-detail/task-detail.component.spec.ts
+++ src/portal/src/app/diagnostics/result-detail/task/task-detail/task-detail.component.spec.ts
@@ -1,12 +1,22 @@
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { TaskDetailComponent } from './task-detail.component';
+import { of } from 'rxjs/observable/of';
 import { MaterialsModule } from '../../../../materials.module';
 import { MatDialogRef, MatDialogModule, MAT_DIALOG_DATA, MatDialog } from '@angular/material';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiService } from '../../../../services/api.service';
 
 class MatDialogModuleMock { }
+
+class ApiServiceStub {
+  static results = [{ nodeName: "testNode", message: { Latency: 513.43, Throughput: 337.58, Detail: "" }, name: 'test', primaryTask: true }];
+
+  diag = {
+    getDiagTaskResult: (jobId: any, taskId: any) => of(ApiServiceStub.results)
+  }
+}
 
 fdescribe('TaskDetailComponent', () => {
   let component: TaskDetailComponent;
@@ -18,7 +28,8 @@ fdescribe('TaskDetailComponent', () => {
       imports: [MaterialsModule, MatDialogModule, NoopAnimationsModule],
       providers: [
         { provide: MatDialogRef, useClass: MatDialogModuleMock },
-        { provide: MAT_DIALOG_DATA, useValue: { msg: { Detail: "message" } } }
+        { provide: MAT_DIALOG_DATA, useValue: { msg: { Detail: "message" } } },
+        { provide: ApiService, useValue: ApiServiceStub }
       ]
     })
       .compileComponents();
@@ -34,5 +45,13 @@ fdescribe('TaskDetailComponent', () => {
     expect(component).toBeTruthy();
     let text = fixture.nativeElement.querySelector(".msg-item").textContent;
     expect(text).toEqual("message");
+  });
+
+  it('should show task detail', () => {
+    let details = fixture.nativeElement.querySelector(".overview-value");
+    let text = details[0].textContent;
+    expect(text).toEqual("testNode");
+    text = details[1].textContent;
+    expect(text).toEqual("513.43");
   });
 });
