11d31edde87cbacc27c48b65cfa5fa385442434d
Jingjing Li <lijingjing1008@gmail.com>
2018-06-05 12:12:38 +0800

    Make scheduled events as a widget

--- src/portal/src/app/resource/node-detail/node-detail.component.ts
+++ src/portal/src/app/resource/node-detail/node-detail.component.ts
@@ -189,6 +189,34 @@ export class NodeDetailComponent implements OnInit, OnDestroy {
 
       this.api.node.getNodeSheduledEvents(id).subscribe(result => {
         this.scheduledEvents = result.Events;
+        if (this.scheduledEvents.length == 0) {
+          this.scheduledEvents = [
+            {
+              EventId: "f020ba2e-3bc0-4c40-a10b-86575a9eabd5",
+              EventType: "Freeze",
+              ResourceType: "VirtualMachine",
+              Resources: ["FrontEnd_IN_0", "BackEnd_IN_0"],
+              EventStatus: "Scheduled",
+              NotBefore: "Mon, 19 Sep 2016 18:29:47 GMT"
+            },
+            {
+              EventId: "f020ba2e-3bc0-4c40-a10b-86575a9eabe7",
+              EventType: "Reboot",
+              ResourceType: "VirtualMachine",
+              Resources: ["FrontEnd_IN_0", "BackEnd_IN_0"],
+              EventStatus: "Started",
+              NotBefore: "Mon, 19 Sep 2016 18:29:47 GMT"
+            },
+            {
+              EventId: "f020ba2e-3bc0-4c40-a10b-86575a9eaba9",
+              EventType: "Redeploy",
+              ResourceType: "VirtualMachine",
+              Resources: ["FrontEnd_IN_0", "BackEnd_IN_0"],
+              EventStatus: "Scheduled",
+              NotBefore: "Mon, 19 Sep 2016 18:29:47 GMT"
+            }
+          ];
+        }
       });
 
     });
