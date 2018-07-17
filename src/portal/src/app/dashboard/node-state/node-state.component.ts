bc99c3f6b47c076a02786fec9e71151db13bb71e
Jingjing Li <lijingjing1008@gmail.com>
2018-07-17 15:58:47 +0800

    Add shade animation when node state data is not ready.

--- src/portal/src/app/dashboard/node-state/node-state.component.ts
+++ src/portal/src/app/dashboard/node-state/node-state.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -6,12 +6,13 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './node-state.component.html',
   styleUrls: ['./node-state.component.scss']
 })
-export class NodeStateComponent implements OnInit {
+export class NodeStateComponent implements OnInit, OnChanges {
   @Input() state: string;
   @Input() stateNum: number;
   @Input() stateIcon: string;
   @Input() total: number;
 
+  private loading = true;
 
   nodesInfo() {
     this.router.navigate(['..', 'resource'], { relativeTo: this.route, queryParams: { filter: this.state } });
@@ -25,4 +26,9 @@ export class NodeStateComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.stateNum !== undefined) {
+      this.loading = false;
+    }
+  }
 }
