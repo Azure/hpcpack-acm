12b9ea65550e8edf13de30f4be7d3ecfd600996a
Evan Cui <dnetdvp@163.com>
2018-08-30 04:10:23 +0800

    Remove the credential

--- src/Common/Utilities/StorageConfiguration.cs
+++ src/Common/Utilities/StorageConfiguration.cs
@@ -2,8 +2,8 @@
 {
     public class StorageConfiguration
     {
-        public string SasToken { get; set; } = "?sv=2017-07-29&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-04-24T18:19:28Z&st=2018-04-25T10:19:28Z&spr=https&sig=pYCVmT40eW54msV7P9F%2BMhBwPUbHr0HYGHvogafCs1I%3D";
-        public string AccountName { get; set; } = "evanchpcacm";
+        public string SasToken { get; set; }
+        public string AccountName { get; set; }
         public string SubscriptionId { get; set; }
         public string ResourceGroup { get; set; }
         public string KeyName { get; set; }
