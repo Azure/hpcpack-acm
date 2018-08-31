86b7c1197f8bd9e5e3e30c4beefe3477bf288849
Jingjing Li <lijingjing1008@gmail.com>
2018-08-31 17:34:37 +0800

    Read cpu information once and get total usage by calculation.

--- src/Metrics/cpu.py
+++ src/Metrics/cpu.py
@@ -1,3 +1,5 @@
 import psutil
 import json
-print(json.dumps(dict([ ("_"+str(index), s) for index, s in enumerate(psutil.cpu_percent(percpu=True,interval=0.1)) ] + [ ("_Total", psutil.cpu_percent()) ])))
+cpuUsages = psutil.cpu_percent(percpu=True,interval=0.1)
+_Total = round(sum(cpuUsages)/len(cpuUsages),2)
+print(json.dumps(dict([ ("_"+str(index), s) for index, s in enumerate(cpuUsages) ] + [ ("_Total", _Total) ])))
