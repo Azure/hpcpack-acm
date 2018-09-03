c696cddce792aa13ba524aa15c701d0cb791b539
Jingjing Li <lijingjing1008@gmail.com>
2018-09-03 18:50:22 +0800

    Format python code.

--- src/Metrics/cpu.py
+++ src/Metrics/cpu.py
@@ -1,5 +1,5 @@
 import psutil
 import json
-cpuUsages = psutil.cpu_percent(percpu=True,interval=0.1)
-_Total = round(sum(cpuUsages)/len(cpuUsages),2)
+cpuUsages = psutil.cpu_percent(percpu=True, interval=0.1)
+_Total = round(sum(cpuUsages) / len(cpuUsages), 2)
 print(json.dumps(dict([ ("_"+str(index), s) for index, s in enumerate(cpuUsages) ] + [ ("_Total", _Total) ])))
