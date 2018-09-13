86502eb61f71c9fd0cc66e9068ae72036a941f7f
Jingjing Li <lijingjing1008@gmail.com>
2018-09-13 16:42:51 +0800

    Add unit tests in dashboard.

--- src/portal/src/app/dashboard/job-overview/job-overview.component.spec.ts
+++ src/portal/src/app/dashboard/job-overview/job-overview.component.spec.ts
@@ -1,25 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ChartModule } from 'angular2-chartjs';
 import { JobOverviewComponent } from './job-overview.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
-describe('JobOverviewComponent', () => {
+fdescribe('JobOverviewComponent', () => {
   let component: JobOverviewComponent;
   let fixture: ComponentFixture<JobOverviewComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  const activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ JobOverviewComponent ]
+      declarations: [
+        JobOverviewComponent
+      ],
+      imports: [
+        ChartModule
+      ],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(JobOverviewComponent);
     component = fixture.componentInstance;
+    component.icon = "Test";
+    component.jobs = {
+      Queued: 10,
+      Running: 1000,
+      Finished: 10000,
+      Canceling: 0,
+      Canceled: 0,
+      Failed: 10
+    };
+    component.jobCategory = "test";
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    let category = fixture.nativeElement.querySelector('.headline .name').textContent;
+    expect(category).toEqual('TEST');
+    let icon = fixture.nativeElement.querySelector('.job').textContent;
+    expect(icon).toEqual('Test');
+    let runningNum = fixture.nativeElement.querySelectorAll('.item-value')[1].textContent;
+    expect(runningNum).toEqual('1000');
   });
 });
--- src/portal/src/app/dashboard/node-state/node-state.component.spec.ts
+++ src/portal/src/app/dashboard/node-state/node-state.component.spec.ts
@@ -1,25 +1,45 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NodeStateComponent } from './node-state.component';
+import { Router, ActivatedRoute } from '@angular/router';
 
-describe('NodeStateComponent', () => {
+fdescribe('NodeStateComponent', () => {
   let component: NodeStateComponent;
   let fixture: ComponentFixture<NodeStateComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+  const activatedRouteSpy = jasmine.createSpyObj('ActivatedRoute', ['']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NodeStateComponent ]
+      declarations: [NodeStateComponent],
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NodeStateComponent);
     component = fixture.componentInstance;
+    component.state = "OK",
+      component.stateNum = 1000;
+    component.stateIcon = "lightbulb_outline";
+    component.total = 1001;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    let icon = fixture.nativeElement.querySelector('.icon .material-icons').textContent;
+    expect(icon).toEqual('lightbulb_outline');
+    let state = fixture.nativeElement.querySelector('.info-title .OK').textContent;
+    expect(state).toEqual(' OK ');
+    let num = fixture.nativeElement.querySelector('.info-content .OK').textContent;
+    expect(num).toEqual('1000');
+    let total = fixture.nativeElement.querySelector('.info-content .total').textContent;
+    expect(total).toEqual('1001');
   });
 });
