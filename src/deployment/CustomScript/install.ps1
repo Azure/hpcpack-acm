09fb5bd8d19b40f38468860f8d9f1993c0a9d042
Evan Cui <dnetdvp@163.com>
2018-12-06 03:42:41 +0800

    Double uninstall

--- src/deployment/CustomScript/install.ps1
+++ src/deployment/CustomScript/install.ps1
@@ -13,5 +13,6 @@ curl -o pack.zip https://evanc.blob.core.windows.net/linuxnm/HpcAcmAgentWin-1.0.
 [System.IO.Compression.ZipFile]::ExtractToDirectory("pack.zip", ".\\pack")
 cd pack
 
+.\handler.ps1 uninstall 2>&1 >> ..\handler.log
 .\handler.ps1 install 2>&1 >> ..\handler.log
 .\handler.ps1 enable 2>&1 >> ..\handler.log
