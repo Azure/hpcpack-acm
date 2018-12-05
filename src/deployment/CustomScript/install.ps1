78b4678f1cb789aa77e0a5ffea2ce8340b620363
Evan Cui <dnetdvp@163.com>
2018-12-06 03:17:04 +0800

    Added install logs

--- src/deployment/CustomScript/install.ps1
+++ src/deployment/CustomScript/install.ps1
@@ -1,8 +1,12 @@
 add-type -AssemblyName System.IO.Compression.FileSystem
-curl -o pack.zip https://evanc.blob.core.windows.net/linuxnm/HpcAcmAgentWin-1.0.2.0.zip
 
 Remove-Item -Path pack -Recurse -Force -ErrorAction Ignore
+
+curl -o pack.zip https://evanc.blob.core.windows.net/linuxnm/HpcAcmAgentWin-1.0.2.0.zip
+
 [System.IO.Compression.ZipFile]::ExtractToDirectory("pack.zip", ".\\pack")
 cd pack
-.\handler.ps1 install
-.\handler.ps1 enable
+
+.\handler.ps1 uninstall 2>&1 >> handler.log
+.\handler.ps1 install 2>&1 >> handler.log
+.\handler.ps1 enable 2>&1 >> handler.log
