66d5d219bc5409ea2968f91a5afa718e2c08b8e1
Evan Cui <dnetdvp@163.com>
2018-12-17 03:17:54 +0800

    fixed the extract path error

--- src/deployment/CustomScript/install.ps1
+++ src/deployment/CustomScript/install.ps1
@@ -10,7 +10,9 @@ if (Test-Path -Path pack)
 
 curl -o pack.zip https://evanc.blob.core.windows.net/linuxnm/HpcAcmAgentWin-1.0.2.0.zip
 
-[System.IO.Compression.ZipFile]::ExtractToDirectory("pack.zip", ".\\pack")
+$pack = Join-Path $PSScriptRoot "pack.zip"
+$packPath = Join-Path $PSScriptRoot "pack"
+[System.IO.Compression.ZipFile]::ExtractToDirectory($pack, $packPath)
 cd pack
 
 .\handler.ps1 uninstall 2>&1 >> ..\handler.log
