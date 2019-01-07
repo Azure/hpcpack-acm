5715762d6417940a0c4b10e2217487a35f6884fd
Evan Cui <dnetdvp@163.com>
2019-01-07 21:24:43 +0800

    Fix the job count issue in error node

--- src/Common/Utilities/CloudUtilitiesExtensions.cs
+++ src/Common/Utilities/CloudUtilitiesExtensions.cs
@@ -215,12 +215,12 @@
                 if (heartbeats.TryGetValue(nodeName, out (ComputeClusterNodeInformation, DateTimeOffset) n))
                 {
                     node.LastHeartbeatTime = n.Item2;
+                    node.RunningJobCount = n.Item1.Jobs.Count;
 
                     if (n.Item2.AddSeconds(u.Option.MaxMissedHeartbeats * u.Option.HeartbeatIntervalSeconds) > DateTimeOffset.UtcNow)
                     {
                         node.Health = NodeHealth.OK;
                         node.State = NodeState.Online;
-                        node.RunningJobCount = n.Item1.Jobs.Count;
                         // TODO: adding events
                     }
                     else
