ad45853ceddce0b49a0af89ad1052852de87676c
zclok010 <zclok010@gmail.com>
2019-04-18 21:44:32 +0800

    Change port from 40000 to 40010 to enable running ACM node manager and HPC Pack node manager on the same node

--- src/Services/NodeAgent/NodeCommunicatorOptions.cs
+++ src/Services/NodeAgent/NodeCommunicatorOptions.cs
@@ -7,6 +7,6 @@
         public int AutoResendLimit { get; set; } = 1;
         public int ResendIntervalSeconds { get; set; } = 3;
         public string AgentUriBase { get; set; } = "http://localhost:5000/api";
-        public string NodeManagerUriBase { get; set; } = "http://localhost:40000/api";
+        public string NodeManagerUriBase { get; set; } = "http://localhost:40010/api";
     }
 }
