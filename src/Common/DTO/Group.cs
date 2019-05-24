5f3709eb7192acb4b0a1095169dfe5bf3cae8b82
Robert Zhang <louirobert@gmail.com>
2019-05-24 09:41:25 +0800

    1) Use "id" instead of "name" in groups URL. 2) "Get /groups" returns nodes within a group 3) Add "Get /groups/{id}" 4) Remove "Get /groups/{name}/nodes"

--- src/Common/DTO/Group.cs
+++ src/Common/DTO/Group.cs
@@ -2,6 +2,8 @@
 {
     public class Group
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
--- src/Frontend/Controllers/GroupsV1Controller.cs
+++ src/Frontend/Controllers/GroupsV1Controller.cs
@@ -15,27 +15,41 @@
         {
             return new Group() { Name = Name, Description = Description, Managed = Managed };
         }
-    }
+}
 
-    [Route("v1/groups")]
+[Route("v1/groups")]
     public class GroupsV1Controller : Controller
     {
         private readonly DataProvider provider;
 
         private List<GroupWithNodes> groups = new List<GroupWithNodes>();
 
+        private int nextId = 0;
+
         public GroupsV1Controller(DataProvider provider)
         {
             this.provider = provider;
-            groups.Add(new GroupWithNodes() { Name = "HeadNodes", Description = "The head nodes in the cluster", Managed = true });
-            groups.Add(new GroupWithNodes() { Name = "ComputeNodes", Description = "The compute nodes in the cluster", Managed = true });
-            groups.Add(new GroupWithNodes() { Name = "LinuxNodes", Description = "The linux nodes in the cluster", Managed = true });
+            groups.Add(new GroupWithNodes() { Id = nextId++, Name = "HeadNodes", Description = "The head nodes in the cluster", Managed = true });
+            groups.Add(new GroupWithNodes() { Id = nextId++, Name = "ComputeNodes", Description = "The compute nodes in the cluster", Managed = true });
+            groups.Add(new GroupWithNodes() { Id = nextId++, Name = "LinuxNodes", Description = "The linux nodes in the cluster", Managed = true });
         }
 
         [HttpGet()]
         public ActionResult GetGroups()
         {
-            return new OkObjectResult(groups.Select(g => g.ToGroup()));
+            return new OkObjectResult(groups);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetGroup(int id)
+        {
+            var idx = groups.FindIndex(g => g.Id == id);
+            if (idx < 0)
+            {
+                return NotFound($"Group {id} is not found!");
+            }
+            return new OkObjectResult(groups[idx]);
         }
 
         [HttpPost]
@@ -49,19 +63,19 @@
             {
                 return BadRequest("Duplicate group name!");
             }
-            var newGroup = new GroupWithNodes() { Name = group.Name, Description = group.Description, Managed = false };
+            var newGroup = new GroupWithNodes() { Id = nextId++, Name = group.Name, Description = group.Description, Managed = false };
             groups.Add(newGroup);
             return new OkObjectResult(newGroup.ToGroup());
         }
 
         [HttpPut] 
-        [Route("{name}")]
-        public ActionResult UpdateGroup(string name, [FromBody] Group update)
+        [Route("{id}")]
+        public ActionResult UpdateGroup(int id, [FromBody] Group update)
         {
-            var idx = groups.FindIndex(g => g.Name == name);
+            var idx = groups.FindIndex(g => g.Id == id);
             if (idx < 0)
             {
-                return NotFound($"Group {name} is not found!");
+                return NotFound($"Group {id} is not found!");
             }
             var group = groups[idx];
             if (group.Managed)
@@ -72,45 +86,36 @@
             {
                 return BadRequest("Group name must not be blank!");
             }
-            update.Managed = false;
+            if (groups.Any(g => g.Name == update.Name && g.Id != id))
+            {
+                return BadRequest($"Duplicate group name {update.Name}!");
+            }
             group.Name = update.Name;
             group.Description = update.Description;
             return new OkObjectResult(group.ToGroup());
         }
 
         [HttpDelete]
-        [Route("{name}")]
-        public ActionResult DeleteGroup(string name)
+        [Route("{id}")]
+        public ActionResult DeleteGroup(int id)
         {
-            var idx = groups.FindIndex(g => g.Name == name);
+            var idx = groups.FindIndex(g => g.Id == id);
             if (idx < 0)
             {
-                return NotFound($"Group {name} is not found!");
+                return NotFound($"Group {id} is not found!");
             }
             groups.RemoveAt(idx);
             return Ok();
         }
 
-        [HttpGet]
-        [Route("{name}/nodes")]
-        public ActionResult GetNodesOfGroup(string name)
-        {
-            var idx = groups.FindIndex(g => g.Name == name);
-            if (idx < 0)
-            {
-                return NotFound($"Group {name} is not found!");
-            }
-            return new OkObjectResult(groups[idx].Nodes.ToList());
-        }
-
         [HttpPost]
-        [Route("{name}/nodes")]
-        public ActionResult AddNodesToGroup(string name, [FromBody] string[] nodeNames)
+        [Route("{id}/nodes")]
+        public ActionResult AddNodesToGroup(int id, [FromBody] string[] nodeNames)
         {
-            var idx = groups.FindIndex(g => g.Name == name);
+            var idx = groups.FindIndex(g => g.Id == id);
             if (idx < 0)
             {
-                return NotFound($"Group {name} is not found!");
+                return NotFound($"Group {id} is not found!");
             }
             var group = groups[idx];
             group.Nodes.UnionWith(nodeNames);
@@ -118,13 +123,13 @@
         }
 
         [HttpDelete]
-        [Route("{name}/nodes")]
-        public ActionResult RemoveNodesFromGroup(string name, [FromBody] string[] nodeNames)
+        [Route("{id}/nodes")]
+        public ActionResult RemoveNodesFromGroup(int id, [FromBody] string[] nodeNames)
         {
-            var idx = groups.FindIndex(g => g.Name == name);
+            var idx = groups.FindIndex(g => g.Id == id);
             if (idx < 0)
             {
-                return NotFound($"Group {name} is not found!");
+                return NotFound($"Group {id} is not found!");
             }
             var group = groups[idx];
             group.Nodes.ExceptWith(nodeNames);
